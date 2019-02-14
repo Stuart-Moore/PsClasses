f13157d375b8cf3488eb3289334ee6c137cb521d
stuart moore <stuart@stuart-moore.com>
2019-02-14 14:56:25 +0000

    pre pres1

--- PsClasses.ps1
+++ PsClasses.ps1
@@ -1,10 +1,49 @@
+#Objects all the way down:
+#region
+$string = 'Hello World'
+$string | Get-Member
+
+$date = Get-Date
+$date | Get-Member
+
+#PsCustomObjects issues
+$obj1 = [PsCustomObject]@{
+    PsTypeName = 'TestObj'
+    fname = 'stuart'
+    sname = 'moore'
+    birthdate = '12/11/1975'
+}
+
+$obj2 = [PsCustomObject]@{
+    PsTypeName = 'TestObj'
+    fname = 'stuart'
+    sname = 'moore'
+    bertdate = '12/11/1975'
+}
+
+$obj1.PsObject.TypeNames
+$obj2.PsObject.TypeNames
+
+function Get-Birthdate {
+    param (
+        [PsTypeName('TestObj')]$inputObject
+    )
+    "Birthdate is $($inputObject.BirthDate)"
+}
+
+Get-BirthDate -inputObject $obj1
+Get-BirthDate -inputObject $obj2
+#endregion
+
+#Classes - structure for a new object
+
 class Person {
     [string] $FirstName
     [String] $SurName
     [datetime] $DateOfBirth
 }
 
-#Create a new variable with New-Object
+#Create a new Object with New-Object
 $p = New-Object Person  
 
 $p
@@ -118,11 +157,48 @@ function Get-YearsOld {
 
 Get-YearsOld $p6
 
+#Can we do this is PsCustomObject?
+
+$personObject = [PsCustomObject]@{
+    PsTypeName = 'PersonObject'
+    FirstName = 'Stuart'
+    Surname = 'Moore'
+    DateOfBirth = [DateTime]'12/11/1975'
+}
+$sb = {
+    return [Math]::Round(((Get-Date)-$This.DateOfBirth).days/365)
+}
+
+$method = @{
+    MemberType = "ScriptMethod"
+    InputObject = $personObject
+    Name = "YearsOld"
+    Value = $sb
+}
+
+Add-Member @method
+
+$personObject
+$personObject.YearsOld()
+$personObject.PsObject.TypeNames 
+function Get-ObjYearsOld {
+    param(
+        [PsTypeName('PersonObject')]$person
+    )
+    "$($Person.FirstName) is $($Person.YearsOld()) old"
+}
+
+Get-ObjYearsOld -person $personObject
+Get-ObjYearsOld -person $p
+
 #Inheritance
 
 class Staff : Person {
     [string] $department
     [DateTime] $hireDate
+    [int]YearsOfService() {
+        return [Math]::Round((((Get-Date)-$this.HireDate).days)/365)
+    }
     Staff (){}
     Staff ([string]$FirstName, [string]$Surname){
         $this.FirstName = $FirstName
@@ -135,10 +211,66 @@ class Staff : Person {
     }
 }
 
+Enum StudentDepartment {
+    Mathematics;
+    Chemistry;
+    Arts;
+    History;
+}
 class Student : Person {
-
+    [StudentDepartment]$department
+    [DateTime]$enrolmentDate
+    [int]YearsOfStudy() {
+        return [Math]::Round((((Get-Date)-$this.enrolmentDate).days)/365)
+    }
+    Student (){}
+    Student ([string]$FirstName, [string]$Surname){
+        $this.FirstName = $FirstName
+        $this.SurName = $Surname
+    }
+    Student ([string]$FirstName, [string]$Surname, [datetime]$DateOfBirth){
+        $this.FirstName = $FirstName
+        $this.SurName = $Surname
+        $this.DateOfBirth = $DateOfBirth
+    }
 }
 
 $s = [Staff]::New('Stuart','Moore','12/11/1975')
+$s.hireDate = '12/09/2016'
 $s.YearsOld()
 $s.Fullname()
+$s.YearsOfService()
+
+$student = [Student]::New('Alice','Brooks','12/02/2000')
+$student.department = 'history'
+
+#Can do complex loads for object build
+class Staff : Person {
+    [string] $department
+    [string] $userid
+    [DateTime] $hireDate
+    [int]YearsOfService() {
+        return [Math]::Round((((Get-Date)-$this.HireDate).days)/365)
+    }
+    Staff (){}
+    Staff ([int]$userid) {
+        $inputObject = ConvertFrom-Csv -InputObject (Get-Content .\MOCK_DATA.csv)  | Where-Object {$_.id -eq $userid}
+        $this.FirstName = $inputObject.first_name
+        $this.SurName = $inputObject.last_name
+        $this.DateOfBirth = [DateTime]$inputObject.DoB
+        $this.userid = "STAFF-$($inputObject.ID)"
+    }
+    Staff ([string]$FirstName, [string]$Surname){
+        $this.FirstName = $FirstName
+        $this.SurName = $Surname
+    }
+    Staff ([string]$FirstName, [string]$Surname, [datetime]$DateOfBirth){
+        $this.FirstName = $FirstName
+        $this.SurName = $Surname
+        $this.DateOfBirth = $DateOfBirth
+    }
+}
+
+$staff = [Staff]::New('7')
+$staff
+
